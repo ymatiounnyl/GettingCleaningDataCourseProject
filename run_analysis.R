46545190e7255d91229fafdd4dae983f86a15897
Youri Matiounine <ymatioun@nyl.com>
2018-02-12 20:13:15 -0500

    a commit

--- run_analysis.R
+++ run_analysis.R
@@ -1,13 +1,26 @@
-# download, unzip and load the data
+# download and unzip the data
 url<-"https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(url,"data.zip")
 unzip("data.zip")
+
+# read all data
 data_test<-read.table("./UCI HAR Dataset/test/X_test.txt")
 data_train<-read.table("./UCI HAR Dataset/train/X_train.txt")
+labels_test<-read.table("./UCI HAR Dataset/test/Y_test.txt")
+labels_train<-read.table("./UCI HAR Dataset/train/Y_train.txt")
 features<-read.table("./UCI HAR Dataset/features.txt")
+activities<-read.table("./UCI HAR Dataset/activity_labels.txt")
+subject_test<-read.table("./UCI HAR Dataset/test/subject_test.txt")
+subject_train<-read.table("./UCI HAR Dataset/train/subject_train.txt")
 
 # combine test and train data
 data_all<-rbind(data_test,data_train)
+labels_all<-rbind(labels_test,labels_train)
+subject_all<-rbind(subject_test,subject_train)
+
+# turn labels into activity names
+activities_all<-merge(activities,labels_all,by.x="V1",by.y="V1")
+activities_all<-activities_all[[2]]
 
 # get indices for means and stds only
 indices<-grep("(mean\\(\\))|(std\\(\\))",features[[2]])
@@ -16,30 +29,21 @@ indices<-grep("(mean\\(\\))|(std\\(\\))",features[[2]])
 data_all<-data_all[,indices]
 features<-features[[2]][indices]
 
-# tidy up names
+# tidy up names and name data columns
 features<-tolower(features)
 features<-gsub("-","",features)
 features<-gsub("\\(","",features)
 features<-gsub("\\)","",features)
-
-# name data columns and save "data_all"
 names(data_all)<-features
-write.table(data_all,"data_all.txt",row.name=FALSE)
 
 
 # From the data set in step 4, creates a second, independent tidy data set
 #  with the average of each variable for each activity and each subject
 
-# read subject IDs
-subject_test<-read.table("./UCI HAR Dataset/test/subject_test.txt")
-subject_train<-read.table("./UCI HAR Dataset/train/subject_train.txt")
-
-# combine test and train subjects
-subject_all<-rbind(subject_test,subject_train)
-
-# final second data set - mean of "data_all" 1 by subject.
-data_subject<-aggregate(data_all,subject_all,mean)
-names(data_subject)[[1]]<-"subject"
+# final second data set - mean of "data_all" by subject and activity.
+data_final<-aggregate(data_all,by=cbind(subject_all,activities_all),mean)
+names(data_final)[[1]]<-"subjects"
+names(data_final)[[2]]<-"activities"
 
 # save final dataset
-write.table(data_subject,"data_subject.txt",row.name=FALSE)
+write.table(data_final,"data_subject.txt",row.name=FALSE)
